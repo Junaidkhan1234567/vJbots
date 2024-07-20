a91214bdf648921b77c655093c059f0413f83f55
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2024-07-20 14:12:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,14 +71,14 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/mobizhub3')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Jpiz-WX76lMzYjJl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+nvmpM5XeI9I0NTk1')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'WOLVERINE_P') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
-PM_SEARCH = bool(environ.get('PM_SEARCH', True))
+PM_SEARCH = bool(environ.get('PM_SEARCH', False))
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
