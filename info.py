f3fe19b1f8e8a03e6c8e30811088b4f218fa3672
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2024-07-25 21:54:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -72,7 +72,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Jpiz-WX76lMzYjJl')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+nvmpM5XeI9I0NTk1')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/wolverine273')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'downloadandpayment') # Support Chat Link Without https:// or @
 
