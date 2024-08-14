2fcb3704687b7a47a27675c0193926c96a80fe66
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2024-08-14 21:06:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+AoC-3ybhhGdiYjE1')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/thinkfilmy')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/wolverine273')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'WOLVERIN_P') # Support Chat Link Without https:// or @
