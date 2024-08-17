af58a0f89a831e9348b5c973753dd64df834c86b
Prinal khan <145959127+Junaidkhan1234567@users.noreply.github.com>
2024-08-17 08:15:13 +0530

    Update Channel.py

--- plugins/Extra/Channel.py
+++ plugins/Extra/Channel.py
@@ -3,11 +3,11 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 CHANNEL_ID = -1001909796512
 
-@Client.on_message(filters.channel & (filters.media | filters.text))
-async def add_button(client, message):
+@Client.on_message(filters.channel & (filters.media | filters.text
+async def add_button(clie
     if message.chat.id == CHANNEL_ID:
         button = InlineKeyboardMarkup(
                 [[
-                 InlineKeyboardButton("🔎Movies Request Group⚡", url="https://t.me/thinkfilmy") 
+                 InlineKeyboardButton("button name", url="link") 
                 ]])
         await message.edit_reply_markup(reply_markup=button)
